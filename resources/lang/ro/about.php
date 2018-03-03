55597e556cc69f212894a0ecf14e48eb6143f72e
Marian Daniel <mariandaniel2497@gmail.com>
2018-03-03 17:00:37 +0200

    let's go

--- resources/lang/ro/about.php
+++ resources/lang/ro/about.php
@@ -4,19 +4,6 @@ return [
 
     'about_title' => 'Despre mine',
     'hi' => 'Salut! Sunt <span>Daniel</span>',
-    'first_paragraph' => 'Ce mult te-am iubit, <strong>Spaicule</strong>!<br>
-Ti-am zis-o de-o mie de ori<br>
-Si ca drept dovada,<br>
-Ti-am adus gramada,<br>
-Vreo 3 basculante, de flori. (2x)<br>
-Te-am luat de la mata d-acasa,<br>
-
-In fusta si far de jupon...<br>
-Ti-am luat si poseta,<br>
-Ti-am luat si jacketa<br>
-Si-o geaca mishto, de nailon.<br>
-Ti-am luat si poseta,<br>
-Ti-am luat si jacketa<br>',
-    'second_paragraph' => 'Da.. cateodata ma plictisesc si eu'
+    'first_paragraph' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam laborum ipsa voluptates facere aspernatur alias rerum perferendis ex vel totam fugiat, voluptatibus corrupti minus laudantium vitae provident sed voluptatem eius laboriosam itaque praesentium id dolorum officia atque. Quae, earum pariatur?'
 
 ];
